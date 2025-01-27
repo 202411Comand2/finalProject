4378a399ec11c8290f62c6050a97dac6cec658d0
Ilya Galuzinskii <IlyaGall01@gmail.com>
2025-01-27 20:36:07 +0300

    add Secret

---
+++ DAL/Secrets.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+   static public class Secrets
+   {
+      static public  readonly string Server1= @"Host=localhost;Port=5432;Database=;Username=;Password=";
+   }
+}
