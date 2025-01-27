73cb87c02966d039ca27176e1313515f384b22fc
Ilya Galuzinskii <IlyaGall01@gmail.com>
2025-01-27 20:49:33 +0300

    add Dal (authTokenR)

---
+++ DAL/Repositories/AuthTokenRepository.cs
@@ -0,0 +1,14 @@
+﻿using DAL.Abstractions;
+using FinalProjectEntityDataBase.Entities;
+
+
+namespace DAL.Repositories
+{
+    public class AuthTokenRepository : BaseRepository<AuthToken>
+    {
+        public AuthTokenRepository(IContextManager manager) : base(manager)
+        {
+
+        }
+    }
+}
