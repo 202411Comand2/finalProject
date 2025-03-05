2d609d1f80b9917c824894dbb4ebac671cc1a3f9
Ilya Galuzinskii <IlyaGall01@gmail.com>
2025-03-05 13:28:26 +0300

    add Test delete shop

--- FinalProjectTests/ProductTests.cs
+++ FinalProjectTests/ProductTests.cs
@@ -13,25 +13,20 @@ namespace FinalProjectTests
         [Test]
         public async Task AddShop()
         {
-            var client = new RestClient("https://localhost:7039");
-            //var request = new RestRequest("/add");
-            //var jwt = await client.GetAsync<string>(request);
-            //TestContext.WriteLine(jwt ?? "null");
-            //Assert.That(jwt, Is.Not.Null);
-
+            var client = new RestClient("https://localhost:7039/Shop");
 
             // Создаем запрос
-            var request = new RestRequest("Shop", Method.Post);
+            var request = new RestRequest("add", Method.Post);
             request.AddHeader("Content-Type", "application/json");
 
             // Создаем объект, который хотим отправить
-            var newUser = new ShopDto
+            var newShop = new ShopDto
             {
                 Name = "shops John Doe",
             };
 
             // Сериализуем объект в JSON и добавляем его в тело запроса
-            request.AddJsonBody(newUser);
+            request.AddJsonBody(newShop);
 
             // Выполняем запрос
             var response = client.Execute<ShopDto>(request);
@@ -40,7 +35,74 @@ namespace FinalProjectTests
             if (response.IsSuccessful)
             {
                 Console.WriteLine("shop created successfully!");
-                Console.WriteLine($"shop ID: {response.Data.Id}");
+                Console.WriteLine($"shop ID: {response.Data}");
+            }
+            else
+            {
+                Console.WriteLine($"shop: {response.ErrorMessage}");
+            }
+        }
+
+        [Test]
+        public async Task Update() 
+        {
+            var client = new RestClient("https://localhost:7039/Shop");
+
+            // Создаем запрос
+            var request = new RestRequest("Update", Method.Put);
+            request.AddHeader("Content-Type", "application/json");
+
+            // Создаем объект, который хотим отправить
+            var updateShop = new UpdateShopDto
+            {
+                Id =1,
+                NewName = "shops John Doe Я обновить магазин",
+            };
+
+            // Сериализуем объект в JSON и добавляем его в тело запроса
+            request.AddJsonBody(updateShop);
+
+            // Выполняем запрос
+            var response = client.Execute<UpdateShopDto>(request);
+
+            // Проверяем ответ
+            if (response.IsSuccessful)
+            {
+                Console.WriteLine("shop created successfully!");
+                Console.WriteLine($"shop ID: {response.Data}");
+            }
+            else
+            {
+                Console.WriteLine($"shop: {response.ErrorMessage}");
+            }
+        }
+
+        [Test]
+        public async Task Delete() 
+        {
+            var client = new RestClient("https://localhost:7039/Shop");
+
+            // Создаем запрос
+            var request = new RestRequest("Delete", Method.Delete);
+            request.AddHeader("Content-Type", "application/json");
+
+            // Создаем объект, который хотим отправить
+            var DeleteShop = new DeleteShopDto
+            {
+                Id = 1,
+            };
+
+            // Сериализуем объект в JSON и добавляем его в тело запроса
+            request.AddJsonBody(DeleteShop);
+
+            // Выполняем запрос
+            var response = client.Execute<UpdateShopDto>(request);
+
+            // Проверяем ответ
+            if (response.IsSuccessful)
+            {
+                Console.WriteLine("shop created successfully!");
+                Console.WriteLine($"shop ID: {response.Data}");
             }
             else
             {
