ee30fbdf3fc88105a5f3cbc5f4deba3fa163307c
Ilya Galuzinskii <IlyaGall01@gmail.com>
2025-04-14 07:18:31 +0300

    it is work with ocelot one comand microservis

--- ProductService/ProductService/ProductAPI/Controllers/ProductController.cs
+++ ProductService/ProductService/ProductAPI/Controllers/ProductController.cs
@@ -6,11 +6,9 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers.Product
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class ProductController(IProductService productService) : ControllerBase()
     {
-
-
 
        
             //[HttpGet]
