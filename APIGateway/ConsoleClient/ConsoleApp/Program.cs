623718d53b8690714f1ed2612e114c7272d9b044
Ilya Galuzinskii <IlyaGall01@gmail.com>
2025-04-14 09:27:03 +0300

    add console aplication gateWay

--- APIGateway/ConsoleClient/ConsoleApp/Program.cs
+++ APIGateway/ConsoleClient/ConsoleApp/Program.cs
@@ -12,8 +12,13 @@ namespace ConsoleApp
                 BaseAddress = new Uri("http://localhost:5010")
             };
 
+
+            // http://localhost:5010/gateway/Product/1
+            // http://localhost:5010/gateway/Product/GetAll подумать над разделением
+
+
             // GET запрос
-            var response = await client.GetAsync("");
+            var response = await client.GetAsync("gateway/Product/GetAll");
             var s = await response.Content.ReadAsStringAsync();
             Console.WriteLine(await response.Content.ReadAsStringAsync());
 
